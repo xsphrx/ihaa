ff341de83f32ba994c94a00fdeac2ff27ec007b9
przemek <przemek.maranowski@gmail.com>
2020-03-07 18:30:23 +0100

    update

--- frontend/src/Form.js
+++ frontend/src/Form.js
@@ -1,64 +1,17 @@
 import { render } from 'react-dom'
 import React, { useState, useCallback } from 'react'
-import { useTransition, animated } from 'react-spring'
 
 
 const Form = (props) => {
   const [index, set] = useState(0)
 
   return (
-   <animated.div style={{ ...props.style, background: '', color: 'grey' }} className="form">
-
-    <div className="box">
-    <form className="">
-     <div class="form-row">
-      <div class="form-group col-md-6">
-        <label for="inputEmail4">Email</label>
-        <input type="email" class="form-control" id="inputEmail4" placeholder="Email"/>
-      </div>
-      <div class="form-group col-md-6">
-        <label for="inputPassword4">Password</label>
-        <input type="password" class="form-control" id="inputPassword4" placeholder="Password"/>
-      </div>
-     </div>
-     <div class="form-group">
-      <label for="inputAddress">Address</label>
-      <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St"/>
-     </div>
-     <div class="form-group">
-      <label for="inputAddress2">Address 2</label>
-      <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"/>
-     </div>
-     <div class="form-row">
-      <div class="form-group col-md-6">
-        <label for="inputCity">City</label>
-        <input type="text" class="form-control" id="inputCity"/>
-      </div>
-      <div class="form-group col-md-4">
-        <label for="inputState">State</label>
-        <select id="inputState" class="form-control">
-          <option selected>Choose...</option>
-          <option>...</option>
-        </select>
-      </div>
-      <div class="form-group col-md-2">
-        <label for="inputZip">Zip</label>
-        <input type="text" class="form-control" id="inputZip"/>
-      </div>
-     </div>
-     <div class="form-group">
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" id="gridCheck"/>
-        <label class="form-check-label" for="gridCheck">
-          Check me out
-        </label>
-      </div>
-     </div>
-     <button type="submit" class="btn btn-primary">Sign in</button>
+     <div className="container">
+     <form class="form-inline">
+       <a href="/"><button class="btn btn-lg btn-outline-success" type="button">Hjem</button></a>
      </form>
-    </div>
+     </div>
 
-    </animated.div>
   )
 }
 
--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,26 +1,36 @@
 import { render } from 'react-dom'
 import React, { useState, useCallback } from 'react'
-import { useTransition, animated } from 'react-spring'
 
 
 const Home = (props) => {
   const [index, set] = useState(0)
 
   return (
-   <animated.div style={{ ...props.style, background: 'white' }} className="page">
-      <div class="container">
-        <div class="row">
-          <div class="mx-auto mt-4">
-            <img src="ihaalogo.jpg" height="400" width="400"/>
-          </div>
-        </div>
-        <div class="row">
-          <div className="mx-auto align-baseline more" onClick={props.nextPage}>
-            MORE
-          </div>
-        </div>
-      </div>
-    </animated.div>
+     <div className="container">
+     <div className="App">
+       <form class="form-inline">
+         <button class="btn btn-lg btn-outline-success" type="button">Hjem</button>
+         <a href="/form"><button class="btn btn-sm btn-outline-secondary" type="button" >Bliv en medlem</button></a>
+       </form>
+       <div className="row">
+         <img src="ihaalogo.png" className="rounded mx-auto d-block " alt="Responsive image" width="450px"/>
+       </div>
+         <div className="align-self-center" >
+         <h3>
+           Hvem vi er
+           <small class="text-muted">og hvad er vigtig til os</small>
+         </h3>
+
+         <div className="align-self-center" >
+         <h3>
+           Hvad vi laver
+           <small class="text-muted">og hvordan du kan vaere en dele af det</small>
+         </h3>
+         </div>
+         </div>
+     </div>
+     </div>
+
   )
 }
 
