186d7591e4c3566b78694d4e88788af06d065d37
przemek <przemek.maranowski@gmail.com>
2020-03-08 14:41:42 +0100

    Updates

---
+++ precache-manifest.5fc5de3ff9a1dc4b2b6e94c9ff661441.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50242e7135cd998ebb9995655934a8c8",
+    "url": "/ihaa/index.html"
+  },
+  {
+    "revision": "e097ae638a2fa2fcde60",
+    "url": "/ihaa/static/css/main.e14dc32b.chunk.css"
+  },
+  {
+    "revision": "28e8043a6696cf4eb78b",
+    "url": "/ihaa/static/js/2.32f99c67.chunk.js"
+  },
+  {
+    "revision": "b4ee14f1b0eb12541a4fed8ba73fd856",
+    "url": "/ihaa/static/js/2.32f99c67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e097ae638a2fa2fcde60",
+    "url": "/ihaa/static/js/main.c6417a4e.chunk.js"
+  },
+  {
+    "revision": "dc701b959548e378adaf",
+    "url": "/ihaa/static/js/runtime-main.8a729029.js"
+  }
+]);
